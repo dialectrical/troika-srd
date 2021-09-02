210d35bddd391b979614de89f35a1a504f2e692a
dialectrical <>
2021-09-02 16:36:26 -0700

    adds basic responsiveness to NavDrawer

--- src/components/styles/StyledDrawer.js
+++ src/components/styles/StyledDrawer.js
@@ -5,6 +5,9 @@ import styled from "styled-components";
 export const StyledDrawer = styled(Drawer)`
   width: 40vw;
   text-align: left;
+  @media (max-width: 960px) {
+    width: 20vw;
+  }
 `;
 
 export const StyledDrawerPaper = styled(Paper)`
@@ -13,4 +16,8 @@ export const StyledDrawerPaper = styled(Paper)`
   border-radius: 0;
   background-color: #f1518d;
   height: theme.spacing(16);
+  @media (max-width: 960px) {
+    width: 20vw;
+    padding-left: 0;
+  }
 `;
