c59766c0fb2b7b7bc20821a19ddfa3d04fd4ceb0
dialectrical <>
2021-09-02 17:48:41 -0700

    removes unused crap

--- src/components/BackgroundPage.js
+++ src/components/BackgroundPage.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import sanityClient from "../client.js";
 import BlockContent from "@sanity/block-content-to-react";
 import { StyledTypography } from "./styles/StyledTypography";
